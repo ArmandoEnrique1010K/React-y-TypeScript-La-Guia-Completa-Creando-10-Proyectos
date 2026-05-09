10368facdb9c8ab5907cd8a12be28a2ed2be6574
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-05-08 22:45:47 -0500

    limpieza de dependencias del proyecto #15

--- 15_quiosco_next/components/products/EditProductForm.tsx
+++ 15_quiosco_next/components/products/EditProductForm.tsx
@@ -1,48 +1,49 @@
-"use client"
+"use client";
 
-import { updateProduct } from "@/actions/update-product-action"
-import { ProductSchema } from "@/src/schema"
-import { useParams, useRouter } from "next/navigation"
-import { toast } from "react-toastify"
+import { updateProduct } from "@/actions/update-product-action";
+import { ProductSchema } from "@/src/schema";
+import { useParams, useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
-export default function EditProductForm({ children }: { children: React.ReactNode }) {
-  const router = useRouter()
-  const params = useParams()
-  const id = +params.id!
+export default function EditProductForm({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const params = useParams();
+  const id = +params.id!;
 
   const handleSubmit = async (formData: FormData) => {
     const data = {
       name: formData.get("name"),
       price: formData.get("price"),
       categoryId: formData.get("categoryId"),
-      image: formData.get("image")
-    }
-    const result = ProductSchema.safeParse(data)
+      image: formData.get("image"),
+    };
+    const result = ProductSchema.safeParse(data);
     if (!result.success) {
-      result.error.issues.forEach(issue => {
-        toast.error(issue.message)
-      })
-      return
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
     }
 
-    const response = await updateProduct(result.data, id)
+    const response = await updateProduct(result.data, id);
     if (response?.errors) {
-      response.errors.forEach(issue => {
-        toast.error(issue.message)
-      })
-      return
+      response.errors.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
     }
 
-    toast.success("Producto Actualizado correctamente")
-    router.push("/admin/products")
-  }
+    toast.success("Producto Actualizado correctamente");
+    router.push("/admin/products");
+  };
 
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
-      <form
-        className="space-y-5"
-        action={handleSubmit}
-      >
+      <form className="space-y-5" action={handleSubmit}>
         {children}
         <input
           type="submit"
@@ -51,5 +52,5 @@ export default function EditProductForm({ children }: { children: React.ReactNod
         />
       </form>
     </div>
-  )
+  );
 }
