9c8a6d31c1ed53fb91d796f63e9eb9aa85082554
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-05-09 17:43:14 -0500

    Actualice prisma 6 a la version 7 para insertar datos en un entorno local

--- 15_quiosco_next/prisma/seed.ts
+++ 15_quiosco_next/prisma/seed.ts
@@ -1,28 +1,26 @@
-import { categories } from "./data/categories"
-import { products } from "./data/products"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+import { categories } from "./data/categories";
+import { products } from "./data/products";
+import { prisma } from "../src/lib/prisma";
 
 async function main() {
   try {
     await prisma.category.createMany({
-      data: categories
-    })
+      data: categories,
+    });
     await prisma.product.createMany({
-      data: products
-    })
+      data: products,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- 15_quiosco_next/src/lib/prisma.ts
+++ 15_quiosco_next/src/lib/prisma.ts
@@ -1,10 +1,27 @@
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  })
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: ["query"],
+//   })
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const connectionString = process.env.DATABASE_URL!;
+
+const pool = new Pool({
+  connectionString,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export { prisma };
